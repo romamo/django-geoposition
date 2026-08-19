b5a85d05b0d6382af388bca2f14d44aa8c7091c6
Romamo <github@romavm.dev>
2026-08-19 11:39:35 -0700

    Fix GeopositionField regex pattern
    - Use raw string for regex pattern in South introspection rules

--- geoposition/models.py
+++ geoposition/models.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 try:
     from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^geoposition\.fields\.GeopositionField"])
+    add_introspection_rules([], [r"^geoposition\.fields\.GeopositionField"])
 except ImportError:
     pass
